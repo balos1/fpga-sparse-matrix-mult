1755a25f5cede6a9a025eaabb21aa12cb680c25f
monootc <33586362+monootc@users.noreply.github.com>
2017-11-15 10:58:00 -0800

    Create async_receiver.sv
    This module receives the bits sent from the serial communication.

---
+++ systemverilog/async_receiver.sv
@@ -0,0 +1,96 @@
+/* This module receives the data sent from the UART rs-232 communication */
+
+module async_receiver(
+	input logic clk,
+	input logic RxD,
+	output logic RxD_data_ready = 0,
+	output logic [7:0] RxD_data = 0,  // data received, valid only (for one clock cycle) when RxD_data_ready is asserted
+
+	// We also detect if a gap occurs in the received stream of characters
+	// That can be useful if multiple characters are sent in burst
+	//  so that multiple characters can be treated as a "packet"
+	output logic RxD_idle,  // asserted when no data has been received for a while
+	output logic RxD_endofpacket = 0  // asserted for one clock cycle when a packet has been detected (i.e. RxD_idle is going high)
+);
+
+parameter ClkFrequency = 50000000; // 50MHz
+parameter Baud = 115200;
+
+parameter Oversampling = 8;  // needs to be a power of 2
+// we oversample the RxD line at a fixed rate to capture each RxD data bit at the "right" time
+// use 16 for higher quality reception
+
+logic [3:0] RxD_state = 0; // keep track of how many bits have received
+
+logic OversamplingTick;
+BaudTickGen #(ClkFrequency, Baud, Oversampling) tickgen(.clk(clk), .enable(1'b1), .tick(OversamplingTick));
+
+// synchronize RxD to our clk domain
+logic [1:0] RxD_sync = 2'b11;
+always @(posedge clk) if(OversamplingTick) RxD_sync <= {RxD_sync[0], RxD};
+
+// and filter it
+logic [1:0] Filter_cnt = 2'b11;
+logic RxD_bit = 1'b1;
+
+always @(posedge clk)
+if(OversamplingTick)
+begin
+	if(RxD_sync[1]==1'b1 && Filter_cnt!=2'b11) Filter_cnt <= Filter_cnt + 1'd1;
+	else 
+	if(RxD_sync[1]==1'b0 && Filter_cnt!=2'b00) Filter_cnt <= Filter_cnt - 1'd1;
+
+	if(Filter_cnt==2'b11) RxD_bit <= 1'b1;
+	else
+	if(Filter_cnt==2'b00) RxD_bit <= 1'b0;
+end
+
+// and decide when is the good time to sample the RxD line
+function integer log2(input integer v); begin log2=0; while(v>>log2) log2=log2+1; end endfunction
+localparam l2o = log2(Oversampling);
+logic [l2o-2:0] OversamplingCnt = 0;
+
+
+always @(posedge clk) if(OversamplingTick) OversamplingCnt <= (RxD_state==0) ? 1'd0 : OversamplingCnt + 1'd1;
+wire sampleNow = OversamplingTick && (OversamplingCnt==Oversampling/2-1);
+
+// now we can accumulate the RxD bits in a shift-register
+always @(posedge clk)
+case(RxD_state)
+	4'b0000: if(~RxD_bit) RxD_state <=  4'b0001;  // start bit found?
+	4'b0001: if(sampleNow) RxD_state <= 4'b1000;  // sync start bit to sampleNow
+	4'b1000: if(sampleNow) RxD_state <= 4'b1001;  // bit 0
+	4'b1001: if(sampleNow) RxD_state <= 4'b1010;  // bit 1
+	4'b1010: if(sampleNow) RxD_state <= 4'b1011;  // bit 2
+	4'b1011: if(sampleNow) RxD_state <= 4'b1100;  // bit 3
+	4'b1100: if(sampleNow) RxD_state <= 4'b1101;  // bit 4
+	4'b1101: if(sampleNow) RxD_state <= 4'b1110;  // bit 5
+	4'b1110: if(sampleNow) RxD_state <= 4'b1111;  // bit 6
+	4'b1111: if(sampleNow) RxD_state <= 4'b0010;  // bit 7
+	4'b0010: if(sampleNow) RxD_state <= 4'b0000;  // stop bit
+	default: RxD_state <= 4'b0000;
+endcase
+
+always @(posedge clk)
+if(sampleNow && RxD_state[3]) RxD_data <= {RxD_bit, RxD_data[7:1]};
+
+always @(posedge clk)
+begin
+	RxD_data_ready <= (sampleNow && RxD_state==4'b0010 && RxD_bit);  // make sure a stop bit is received
+end
+
+
+logic [l2o+1:0] GapCnt = 0;
+
+always @(posedge clk) begin 
+	if (RxD_state!=0) GapCnt<=0; 
+	else if(OversamplingTick & ~GapCnt[log2(Oversampling)+1]) 
+	GapCnt <= GapCnt + 1'h1;
+end
+
+assign RxD_idle = GapCnt[l2o+1];
+
+always @(posedge clk) RxD_endofpacket <= OversamplingTick & ~GapCnt[l2o+1] & &GapCnt[l2o:0];
+
+
+endmodule
