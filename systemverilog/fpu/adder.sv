4268a99631b9f9d9f9c4189a2d49fda91cd41233
Cody Balos <cjbalos@gmail.com>
2017-11-24 02:34:33 -0800

    implemented half precision multiplier

---
+++ systemverilog/fpu/adder.sv
@@ -0,0 +1,160 @@
+/*
+    file: adder.sv
+    author: Cody Balos <cjbalos@gmail.com>
+*/
+
+`define neg 15
+`define exponent 14:10
+`define mantissa 9:0
+
+/*
+    module: adder
+
+    A IEEE754-2008 compliant, sequential adder.
+
+    inputs:
+        clock - clock signal
+        clk_en - indicates that the addition should be performed
+        dataa - left operand
+        datab - right operand
+    outputs:
+        result - A + B
+        sign - indicates negative result
+        overflow - indicates overflow
+        underflow - indicates underflow
+        zero - indicates result == 0
+*/
+module adder(input logic clock, clk_en,
+             input logic [15:0] dataa, datab,
+             output logic [15:0] result,
+             output logic sign, overflow, underflow, zero);
+
+    // Breaking adder into 3 different stages
+    // Using a Mealy FSM to determine determine stage
+    typedef enum logic [3:0]
+    {
+        IDLE, STAGE1, STAGE2, STAGE3
+    } statetype;
+
+    statetype current_state = IDLE;
+    statetype next_state = IDLE;
+
+    // Determine the next state based on current state
+    always_comb
+    begin
+        case(current_state)
+            IDLE: next_state = clk_en ? STAGE1 : IDLE;
+            STAGE1: next_state = STAGE2;
+            STAGE2: next_state = STAGE3;
+            STAGE3: next_state = IDLE;
+            default: next_state = IDLE;
+        endcase
+    end
+
+    logic [15:0] A, B;
+    logic Aneg, Bneg, sumneg;
+    logic [4:0] Aexp, Bexp, sumexp;
+    logic [12:0] Asig, Bsig;
+    logic [12:0] sumsig;
+    logic [4:0] diff;
+
+    // Determine the output of the system in the current state and based on the inputs (Mealy FSM)
+    assign result = {sumneg, sumexp, sumsig[9:0]};
+    assign sign = sumneg;
+    // STEPS FOR ADDING "A" and "B":
+    //   1. If B's exponent > swap A and B
+    //   2. Insert 1 in significand if corresponding exponent not zero
+    //   3. If necessary, un-normalize B so that A and B's exponent are the same
+    //   4. Negate the significand if corresponding sign bit is negative
+    //   5. Compute result of significands
+    //   6. Store sign of result and take absolute value of result
+    //   7. Normalize the result
+    always_ff @(posedge clock)
+    begin
+        case(current_state)
+            IDLE: begin
+                if (clk_en) begin
+                    // STEP 1
+                    if (dataa[`exponent] < datab[`exponent]) begin
+                        A <= datab; B <= dataa;
+                    end else begin
+                        A <= dataa; B <= datab;
+                    end
+                end
+            end
+            STAGE1: begin
+                // STEP 2 - Insert 1 in significand if corresponding exponent not zero
+                Aneg <= A[`neg]; Aexp <= A[`exponent];
+                Bneg <= B[`neg]; Bexp <= B[`exponent];
+
+                // Put a 0 in bits 11 and 12 (later used for sign and overflow).
+                // Put a 1 in bit 10 of significand if exponent is non-zero.
+                // Copy mantissa into remdataaing bits.
+                Asig <= { 2'b0, Aexp ? 1'b1 : 1'b0, A[`mantissa] };
+                Asig <= { 2'b0, Bexp ? 1'b1 : 1'b0, B[`mantissa] };
+
+                // STEP 3 - If necessary, un-normalize B so that A and B's exponent are the same
+                diff <= Aexp - Bexp;
+                Bsig <= Bsig >> diff;
+                // Bexp <= Aexp;
+            end
+            STAGE2: begin
+                // STEP 4 - Negate the significand if corresponding sign bit is negative
+                Asig <= Aneg ? -Asig : Asig;
+                Bsig <= Bneg ? -Bsig : Bsig;
+                // STEP 5 - Compute result of significands
+                sumsig <= Asig + Bsig;
+            end
+            STAGE3: begin
+                // STEP 6 - Store sign of result and take absolute value of result
+                sumneg <= sumsig[12];
+                sumsig <= sumneg ? -sumsig : sumsig;
+                // STEP 7 - Normalize the result (if neccessary)
+                if (sumsig[10]) begin
+                    // CASE: Overflow
+                    sumexp <= Aexp + 1;
+                    sumsig <= sumsig >> 1;
+                    underflow <= 0; overflow <= 1; zero <= 0;
+                end else if (sumsig) begin
+                    // CASE: result is nonzero and did not overflow
+
+                    // Find position of first non-zero digit.
+                    int position = 0; int adjust = 0;
+                    for (int i = 11; i >= 0; i = i - 1 )
+                        if (!position && sumsig[i])
+                            position <= i;
+
+                    // Shift significand and exponent
+                    adjust <= 10 - position;
+                    if (Aexp < adjust) begin
+                        // SUBCASE: Underflow, so set all to zero
+                        sumneg <= 0; sumexp <= 0; sumsig <= 0;
+                        underflow <= 1; overflow <= 0; zero <= 1;
+                    end else begin
+                        sumexp <= Aexp - adjust;
+                        sumsig <= sumsig << adjust;
+                        underflow <= 0; overflow <= 1; zero <= 0;
+                    end
+                end else begin
+                    // CASE: result is zero
+                    sumexp <= 0;
+                    sumsig <= 0;
+                    underflow <= 0; overflow <= 0; zero <= 1;
+                end
+            end
+            default: begin
+                // cover all outputs and intermediates
+                result <= 0; sign <= 0; overflow <= 0; underflow <= 0; zero <= 0;
+                A <= 0; B <= 0; Aneg <= 0; Bneg <= 0; Aexp <= 0; Bexp <= 0; Asig <= 0; Bsig <= 0;
+                sumneg <= 0; sumexp <= 0; sumsig <= 0; diff <= 0;
+            end
+        endcase
+    end
+
+    // Go to the next state
+    always_ff @(posedge clock)
+    begin
+        current_state <= next_state;
+    end
+
+endmodule
---
+++ systemverilog/fpu/mult.sv
@@ -0,0 +1,204 @@
+/*
+    file: mult.sv
+    author: Cody Balos <cjbalos@gmail.com>
+*/
+
+`define sign 15
+`define exponent 14:10
+`define mantissa 9:0
+
+/*
+    module: multiplier
+
+    A IEEE754-2008 compliant, 8-stage multiplier.
+
+    inputs:
+        clock - clock signal
+        clk_en - indicates that the addition should be performed
+        dataa - left operand
+        datab - right operand
+    outputs:
+        result - A + B
+        sign - indicates negative result
+        overflow - indicates overflow
+        underflow - indicates underflow
+        zero - indicates result == 0
+*/
+module mult(input logic clock, clk_en,
+             input logic [15:0] dataa, datab,
+             output logic [15:0] result,
+             output logic sign, overflow, underflow, zero, nan);
+
+    // Breaking adder into 7/8 different stages
+    // Using a Mealy FSM to determine determine stage
+    typedef enum logic [3:0]
+    {
+        IDLE, STAGE0, STAGE1, STAGE2, STAGE3, STAGE4, STAGE5, STAGE6
+    } statetype;
+
+    statetype current_state = IDLE;
+    statetype next_state = IDLE;
+
+    // Determine the next state based on current state
+    always_comb
+    begin
+        case(current_state)
+            IDLE: next_state = clk_en ? STAGE0 : IDLE;
+            STAGE0: next_state = STAGE1;
+            STAGE1: next_state = STAGE2;
+            STAGE2: next_state = STAGE3;
+            STAGE3: next_state = STAGE4;
+            STAGE4: next_state = STAGE5;
+            STAGE5: next_state = STAGE6;
+            STAGE6: next_state = IDLE;
+            default: next_state = IDLE;
+        endcase
+    end
+
+    logic a_sin, b_sin, result_sin;
+    logic [6:0] a_exp, b_exp, result_exp;
+    logic [10:0] a_man, b_man, result_man;
+    logic [21:0] man_product;
+    logic guard, sticky, round;
+
+    // Determine outputs
+    always_ff @(posedge clock)
+    begin
+        case(current_state)
+            IDLE: begin
+                if (clk_en) begin
+                    // Unpack operands
+                    a_sin <= dataa[`sign]; b_sin <= datab[`sign];
+                    a_exp <= dataa[`exponent]; b_exp <= datab[`exponent];
+                    a_man <= dataa[`mantissa]; b_man <= datab[`mantissa];
+                end
+            end
+            STAGE0: begin
+                // STEP 0. denormalize
+                if ($signed(a_exp) == -15) begin
+                    a_exp <= -14;
+                end else begin
+                    a_man[10] <= 1;
+                end
+                if ($signed(b_exp) == -15) begin
+                    b_exp <= -14;
+                end else begin
+                    b_man[10] <= 1;
+                end
+            end
+            STAGE1: begin
+                // STEP 1. normalize A and B
+                if (a_man[10] == 0) begin
+                    a_man <= a_man << 1;
+                    a_exp <= a_exp - 1;
+                end
+                if (b_man[10] == 0) begin
+                    b_man <= b_man << 1;
+                    b_exp <= b_exp - 1;
+                end
+            end
+            STAGE2: begin
+                // STEP 2. calculate sign of result
+                result_sin <= a_sin^b_sin;
+                // STEP 3. multiply mantissas
+                man_product <= a_man*b_man;
+                // STEP 4. calculate tentative exponent
+                result_exp <= a_exp + b_exp - 15;
+            end
+            STAGE3: begin
+                // STEP 5. calculate guard, round, sticky
+                result_man <= man_product[21:12];
+                guard <= man_product[11];
+                round <= man_product[10];
+                sticky <= man_product[9:0] != 0;
+            end
+            STAGE4: begin
+                // STEP 6. normalize result
+                if (result_man[10] == 0) begin
+                    result_man <= result_man << 1;
+                    result_exp <= result_exp - 1;
+                    result_man[0] <= guard;
+                    guard <= round;
+                    round <= 0;
+                end else if ($signed(result_exp) < -14) begin
+                    result_man <= result_man >> 1;
+                    result_exp <= result_exp + 1;
+                    guard <= result_man[0];
+                    round <= guard;
+                    sticky <= sticky | round;
+                end
+            end
+            STAGE5: begin
+                // STEP 7. round
+                if (guard && (round | sticky | result_man)) begin
+                    result_man <= result_man + 1;
+                    if (result_man == 10'hfff) begin
+                        result_exp <= result_exp + 1;
+                    end
+                end
+            end
+            STAGE6: begin
+                // STEP 8. Determine final output
+                if ((a_exp == 15 && a_man != 0) || (b_exp && b_man != 0)) begin
+                    // case where one or both inputs are NaN
+                    nan <= 1;
+                    result <= {1'b1, 5'd32, 10'h200};
+                end else if (a_exp == 16) begin
+                    // case when A is infty
+                    if ($signed(b_exp == -15) && (b_man == 0)) begin
+                        // B is zero --> 0*infty = NaN
+                        nan <= 1;
+                        result <= {1'b1, 5'd32, 10'h200};
+                    end else begin
+                        // B is not zero --> N*infty = infty
+                        result <= {result_sin, 5'd32, 10'd0};
+                    end
+                end else if (b_exp == 16) begin
+                    // case when B is infty
+                    if ($signed(a_exp == -15) && (a_man == 0)) begin
+                        // A is zero --> infty*0 = NaN
+                        nan <= 1;
+                        result <= {1'b1, 5'd32, 10'h200};
+                    end else begin
+                        // A is not zero --> N*infty = infty
+                        result <= {result_sin, 5'd32, 10'd0};
+                    end
+                end else if (($signed(a_exp) == -15) && (a_man == 0)) begin
+                    // case when A iz zero
+                    result <= {result_sin, 5'd0, 10'd0};
+                end else if (($signed(b_exp) == -15) && (b_man == 0)) begin
+                    // case when B iz zero
+                    result <= {result_sin, 5'd0, 10'd0};
+                end else if ($signed(result_exp) < -14) begin
+                    // in case of underflow, return 0
+                    underflow <= 1;
+                    result <= {result_sin, 5'd0, 10'd0};
+                end else if ($signed(result_exp) > 15) begin
+                    // in case of overflow, return infty
+                    overflow <= 1;
+                    result <= {result_sin, 5'd32, 10'd0};
+                end else begin
+                    result <= {result_sin, result_exp[4:0] + 15, result_man[9:0]};
+                    // handle case where exponent in result is the minimum
+                    if ($signed(result_exp) == -14 && result_man[10] == 0) begin
+                        result[`exponent] <= 0;
+                    end
+                end
+            end
+            default: begin
+                // cover all outputs and intermediates
+            end
+        endcase
+    end
+
+    // zero flag is much easier to determine combinationally
+	 assign sign = result[`sign];
+    assign zero = (result[`exponent] == 0) && (result[`mantissa] == 0);
+
+    // Go to the next state
+    always_ff @(posedge clock)
+    begin
+        current_state <= next_state;
+    end
+
+endmodule
