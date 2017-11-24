5a1e72a6dead4d78a639e5295b4d27746ff4b378
Cody Balos <cjbalos@gmail.com>
2017-11-24 02:35:26 -0800

    created quartus project for system, and created single precision multiplier using IP library

---
+++ systemverilog/fpu/multiplier_inst.v
@@ -0,0 +1,11 @@
+multiplier	multiplier_inst (
+	.clk_en ( clk_en_sig ),
+	.clock ( clock_sig ),
+	.dataa ( dataa_sig ),
+	.datab ( datab_sig ),
+	.nan ( nan_sig ),
+	.overflow ( overflow_sig ),
+	.result ( result_sig ),
+	.underflow ( underflow_sig ),
+	.zero ( zero_sig )
+	);
