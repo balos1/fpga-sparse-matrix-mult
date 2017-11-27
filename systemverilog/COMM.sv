e3bf5176ea219ddd38876ddc102d8a0389b4c59f
Cody Balos <rbalos@STK.PACIFIC.EDU>
2017-11-27 15:15:04 -0800

    created comm module and started connecting pieces together

--- systemverilog/COMM.sv
+++ systemverilog/COMM.sv
@@ -1,18 +1,55 @@
-module COMM(input logic clk, RxD,
-				input logic [7:0] GPin,//general purspose inputs
-				output logic TxD,
-				output logic [7:0] GPout); //general purpose output
+
+
+
+module comm(
+	input logic clk, reset, wen,
+	input logic RxD,
+	input logic TxD_start,
+	output logic TxD,
+	output logic TxD_busy
+);
 	
+	logic ready;
 	logic RxD_data_ready;
 	logic [7:0] RxD_data;
-	async_receiver RX(.clk(clk), .RxD(RxD), .RxD_data_ready(RxD_data_ready), .RxD_data(RxD_data));
+	logic [15:0] writePtr;
+	logic [15:0] readPtr;
+	logic [7:0] outData;
+
+
+	control ctl
+	(
+		.clk(clk), 
+		.reset(reset),
+		.wen(wen),
+		.dataReady(RxD_data_ready),
+		.inByte(RxD_data),
+		.ready(ready),
+		.writePtr(writePtr),
+		.readPtr(readPtr),
+		.outData(outData)
+	);
 	
-	always @(posedge clk)
-	begin
-	if(RxD_data_ready)
-		GPout <= RxD_data;
-	end
 	
-	async_transmitter TX(.clk(clk), .TxD(TxD), .TxD_start(RxD_data_ready), .TxD_data(GPin));
-		
-endmodule
+	// INSTANTIATE BUFFER/MEM
+
+	async_transmitter #(50000000, 115200)
+	(
+		.clk(clk),
+		.TxD_start(TxD_start),
+		.TxD(TxD),
+		.TxD_busy(TxD_busy)
+	);
+	
+	async_receiver #(50000000, 115200, 16)
+	(
+		.clk(clk),
+		.RxD(RxD),
+		.RxD_data_ready(RxD_data_ready),
+		.RxD_data(RxD_data),
+		.RxD_idle(),
+		.RxD_endofpacket()
+	);
+	
+
+endmodule;
