da99334cbaaa83c406c04a8bb0ee324e75671945
Cody Balos <cjbalos@gmail.com>
2017-11-28 11:00:57 -0800

    working on python script that will mock the host

--- mockhost/mockhost.py
+++ mockhost/mockhost.py
@@ -6,31 +6,54 @@ Python program which mocks a host system that uses Cyclone IV E FPGA as a coproc
 author: Cody Balos <cjbalos@gmail.com>
 """
 
-import serial
 import numpy as np
 from scipy import sparse
+import serial
+import struct
 import time
+from threading import Thread
 
 PORT = 'loop://'
-TIMEOUT = 10000
+TIMEOUT = 100
+
+ser = serial.serial_for_url(PORT, timeout=TIMEOUT)
 
 def main():
-    uart = CommUnit(PORT, TIMEOUT)
-    A = np.matrix([[1.0, 0.0, 1.2], [0.0, 0.0, 2.0], [0.0, 3.0, 0.0]])
-    B = np.matrix([[0.0, 2.0, 3.2], [1.0, 0.0, 0.0], [0.0, 2.2, 0.0]])
-    uart.send_matrices(A, B)
+    uart = CommUnit(ser)
+
+    A = np.matrix([[1.0, 0.0, 1.2], [0.0, 0.0, 2.0], [0.0, 3.0, 0.0]], dtype=np.float16)
+    B = np.matrix([[0.0, 2.0, 3.2], [1.0, 0.0, 0.0], [0.0, 2.2, 0.0]], dtype=np.float16)
+    # uart.send_matrices(A, B)
+
+    thread1 = Thread(target = uart.send_matrices, args=(A,B))
+    thread2 = Thread(target = test_sender)
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    time.sleep(2)
+    exit()
+
+def test_sender():
+    # print("test")
+    data = b''
+    buffer = bytearray()
+    while b'\n' not in data:
+        data = ser.read()
+        print(data)
+        # buffer.append()
+    # print(buffer)
 
 class CommUnit():
     """
     """
 
-    def __init__(self, port, timeout):
-        self.uart = serial.serial_for_url(port, timeout=timeout)
-        self.uart.baudrate = 115200
-        self.uart.parity = serial.PARITY_NONE
-        self.uart.bytesize = serial.EIGHTBITS
-        self.uart.stopbits = serial.STOPBITS_ONE
-        self.uart.rtscts = True
+    def __init__(self, ser):
+        self.ser = ser
+        self.ser.baudrate = 115200
+        self.ser.parity = serial.PARITY_NONE
+        self.ser.bytesize = serial.EIGHTBITS
+        self.ser.stopbits = serial.STOPBITS_ONE
+        self.ser.rtscts = False
 
     def send_matrices(self, A, B):
         """
@@ -41,12 +64,19 @@ class CommUnit():
         sB = sparse.csc_matrix(B)
         sparse.csc_matrix.sort_indices(sB)
 
+        value_buffer = bytearray()
+        for value in sA.data:
+            print(struct.pack('e', value))
+        # print(value_buffer)
 
+        index_buffer = bytearray()
+        for index in sA.indices:
+            index_buffer.append(index)
 
-        # print("sA = ")
-        # print(sA)
-        # print("sB = ")
-        # print(sB)
+        # self.ser.write(index_buffer)
+        # self.ser.write(b'\n')
+        time.sleep(0.1)
+
 
 if __name__ == "__main__":
     main()
