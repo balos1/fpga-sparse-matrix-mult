fe5b8830a7832ba8734f9523048a29ffa96bbfe5
CrystalBS <33564747+CrystalBS@users.noreply.github.com>
2017-11-29 14:55:23 -0800

    Updating top module

--- systemverilog/sparse_matrix_coprocessor.sv
+++ systemverilog/sparse_matrix_coprocessor.sv
@@ -1,7 +1,10 @@
 module sparse_matrix_coprocessor(
-	input logic clk,
+	input logic clk, en,
+	output logic [15:0]result,
+	output logic overflow, underflow, zero, nan
 	
 );
+
 comm c(
 //	input logic clk, reset, wen,
 //	input logic RxD,
@@ -17,7 +20,7 @@ comm c(
 	.TxD_busy()
 );
 
-PIC p(
+PIC p1( 	//should have multiple of these
 //module PIC (input logic clk,
 //				input logic [2:0] A0, B0, //data and index
 //				output logic eq,
@@ -25,55 +28,56 @@ PIC p(
 	.clk(clk),
 	.A0(),
 	.B0(),
-	.eq(eq),
+	.eq(),
 	.dataOut()
 );
 
-fpu/mult m(
-//	module mult(input logic clock, reset, clk_en,
-//            input logic [15:0] dataa, datab,
-//            output logic [15:0] result,
-//            output logic overflow, underflow, nan);
-	.clock(clk),
-	.reset(),
-	.clk_en(),
-	.dataa(),
-	.datab(),
-	.result(),
-	.overflow(),
-	.underflow(),
-	.nan()
-);
+//fpu/mult m(
+////	module mult(input logic clock, reset, clk_en,
+////            input logic [15:0] dataa, datab,
+////            output logic [15:0] result,
+////            output logic overflow, underflow, nan);
+//	.clock(clk),
+//	.reset(),
+//	.clk_en(),
+//	.dataa(),
+//	.datab(),
+//	.result(),
+//	.overflow(),
+//	.underflow(),
+//	.nan()
+//);
+//
+//
+//fpu/adder add(
+////	module adder(input logic clock, reset, clk_en,
+////             input logic [15:0] dataa, datab,
+////             output logic [15:0] result,
+////             output logic overflow, underflow);
+//	.clock(clk),
+//	.reset(),
+//	.clk_en(),
+//	.dataa(),
+//	.datab(),
+//	.result(),
+//	.overflow(),
+//	.underflow()
+//);
 
-fpu/fpu f(
+fpu/fpu f(  
 //	module fpu(input logic clk, clk_en,
 //				  input logic [15:0] dataa, datab,
 //				  output logic [15:0] result,
-//				  output logic sign, overflow, underflow, zero, nan);
+//				  output logic sign, overflow, underflow, zero, nan); //implements both adder and mult?
 	.clk(clk),
-	.clk_en(),
+	.clk_en(en),
 	.dataa(),
 	.datab(),
-	.result(),
-	.overflow(),
-	.underflow(),
-	.zero(),
-	.nan()
-);
-
-fpu/adder add(
-//	module adder(input logic clock, reset, clk_en,
-//             input logic [15:0] dataa, datab,
-//             output logic [15:0] result,
-//             output logic overflow, underflow);
-	.clock(clk),
-	.reset(),
-	.clk_en(),
-	.dataa(),
-	.datab(),
-	.result(),
-	.overflow(),
-	.underflow()
+	.result(result),
+	.overflow(overflow),
+	.underflow(underflow),
+	.zero(zero),
+	.nan(nan)
 );
 
 endmodule 
