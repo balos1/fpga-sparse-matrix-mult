7f9548df1ed0b526507ac3ce11f27d1b2c8e4667
monootc <33586362+monootc@users.noreply.github.com>
2017-11-29 20:24:19 -0800

    Update control.sv
    Now each memory slot contains the value and the row indices

--- systemverilog/control.sv
+++ systemverilog/control.sv
@@ -9,7 +9,7 @@ module control(input logic clk, reset, wen, dataReady,
 					output logic [15:0] writePtr,
 					output logic [15:0] readPtr,
 					output logic [7:0] outData);
-
+parameter number_of_bytes = 16;
 integer byteCounter = 0;
 typedef enum logic [3:0] {idle, writing, writeReady, zero, hold, read} State;
 State curState = idle;
@@ -36,9 +36,9 @@ always_comb begin
 						else
 							nextState = idle;
 		
-		writing:		if(byteCounter < 4 && dataReady)
+		writing:		if(byteCounter < number_of_bytes && dataReady)
 							nextState = writing;
-						else if(byteCounter == 4)
+						else if(byteCounter == number_of_bytes)
 							nextState = writeReady;
 						else
 							nextState = hold;
@@ -66,14 +66,14 @@ end
 
 always_ff @(posedge clk) begin
 	case(curState)
-			idle:		begin			
-						writePtr <= 0;
-						readPtr <= 0;
-					end
+			idle:			begin			
+								writePtr <= 0;
+								readPtr <= 0;
+							end
 			writeReady:	begin
-						writePtr <= writePtr + 1;
-						//byteCounter <= 0;
-					end
+								writePtr <= writePtr + 1;
+								//byteCounter <= 0;
+							end
 							
 			read:			readPtr <= readPtr + 1;
 	endcase
@@ -83,7 +83,7 @@ always_ff @(posedge clk) begin
 	if (curState == writing)
 		byteCounter <= byteCounter + 1;
 	else if(inByte == 0)
-		byteCounter <= 4;
+		byteCounter <= number_of_bytes;
 	else if(curState == writeReady)
 		byteCounter <= 0;
 end
@@ -111,4 +111,3 @@ endmodule
 
 
 		
-
